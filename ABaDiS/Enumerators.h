b1f593bf3fb78f2a79b98f468abb45d6d9a47607
KopogtiasVH <xtremefeel@gmx.de>
2018-07-17 15:02:12 +0200

    Revert "Revert "added the enumerators class to deal with the tons of enums and the problem of impossible passing""
    This reverts commit 6e6ce1a42a64d75f5cc506fe913477560fdc19d1.

---
+++ ABaDiS/Enumerators.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "stdafx.h"
+
+class Enumerators
+{
+public:
+	// The classes for SecOfficers
+	enum class OfficerClass {
+		riotSpecialist,
+		detective,
+		commander,
+		officer
+	};
+
+};
+
