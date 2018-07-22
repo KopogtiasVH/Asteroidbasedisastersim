3b76fcce7cbe2104ff7d0981ec00a31b27e0546c
KopogtiasVH <xtremefeel@gmx.de>
2018-07-22 12:48:25 +0200

    made it compile again

--- ABaDiS/Battle.cpp
+++ ABaDiS/Battle.cpp
@@ -28,16 +28,20 @@ Battle::Battle(Leader* a, Leader* b, Room* r)
 
 bool Battle::prepareNextRound() {
 	// Fill the Row Vectors with new Units to max out the frontWidth
+	return true;
 }
 
 bool Battle::calculateRound() {
 	// Calculate and distribute the Damage
+	return true;
 }
 
 bool Battle::endRound() {
 	// Empty out fallen and fleeing Units and check if the fight is going for another round
+	return true;
 }
 
 bool Battle::aftermath() {
 	// Tell the Basesystem and the participating Leaders the outcome of the battle
+	return true;
 }
