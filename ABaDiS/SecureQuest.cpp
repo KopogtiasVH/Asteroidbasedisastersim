433b64d980ffea40e93bd80e8d1b39ccff62bdf7
KopogtiasVH <xtremefeel@gmx.de>
2018-09-12 15:04:06 +0200

    implemented securing quests

--- ABaDiS/SecureQuest.cpp
+++ ABaDiS/SecureQuest.cpp
@@ -2,9 +2,62 @@
 #include "SecureQuest.h"
 
 
-SecureQuest::SecureQuest()
+SecureQuest::SecureQuest(Being* c, Room* tS) : MilitaryQuest()
+{
+	client = c;
+	toSecure = tS;
+	ownFaction = Enumerators::Faction::NONE;
+	otherFaction = Enumerators::Faction::NONE;
+	createQuestFlavor();
+}
+
+void SecureQuest::createQuestFlavor()
+{
+	switch (ownFaction) {
+	case Enumerators::Faction::NONE:
+		name = "Secure Quest";
+		description = name;
+		break;
+	case Enumerators::Faction::ANARC:
+		name = "Secure " + toSecure->getName() + ".";
+		description = "Clean " + toSecure->getName() + " of all Security presence and report back to " + client->getSurName() + ".";
+		break;
+	case Enumerators::Faction::SEC:
+		name = "Secure " + toSecure->getName() + ".";
+		description = "Clean " + toSecure->getName() + " of all Anarchist presence and report back to " + client->getSurName() + ".";
+		break;
+	default:
+		std::cerr << "Wrong Faction assigned" << std::endl;
+		break;
+	}
+}
+
+void SecureQuest::assembleReward()
 {
 }
 
+bool SecureQuest::checkProgress()
+{
+	return !toSecure->isPresent(otherFaction);
+}
 
-
+void SecureQuest::activateQuest(Being * o)
+{
+	owner = o;
+	if (dynamic_cast<Leader*>(o)) {
+		Leader* oL = dynamic_cast<Leader*>(o);
+		ownFaction = oL->getFaction();
+		switch (ownFaction) {
+		case Enumerators::Faction::ANARC:
+			otherFaction = Enumerators::Faction::SEC;
+			break;
+		case Enumerators::Faction::SEC:
+			otherFaction = Enumerators::Faction::ANARC;
+			break;
+		default:
+			std::cerr << "False ownFaction assigned" << std::endl;
+			break;
+		}
+	}
+	createQuestFlavor();
+}
--- ABaDiS/SecureQuest.h
+++ ABaDiS/SecureQuest.h
@@ -4,6 +4,17 @@ class SecureQuest :
 	public MilitaryQuest
 {
 public:
-	SecureQuest();
+	SecureQuest(Being*, Room*);
+
+	void createQuestFlavor();
+	void assembleReward();
+	bool checkProgress();
+	void activateQuest(Being*);
+
+private:
+	Being* client;
+	Room* toSecure;
+	Enumerators::Faction ownFaction;
+	Enumerators::Faction otherFaction;
 };
 
